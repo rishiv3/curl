1c8bc61c8440f89d4529153221720ea3016504c5
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2017-03-16 11:50:57 +0530

    index file added with code

---
+++ curl.php
@@ -0,0 +1,39 @@
+<?php
+// error reporting
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//setting url
+$url = 'http://example.com/api';
+
+//data
+$data = array("message" => "Hello World!!!");
+try {
+    $ch = curl_init($url);
+    $data_string = json_encode($data);
+
+    if (FALSE === $ch)
+        throw new Exception('failed to initialize');
+
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json', 'Content-Length: ' . strlen($data_string)));
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+    	$output = curl_exec($ch);
+
+    if (FALSE === $output)
+        throw new Exception(curl_error($ch), curl_errno($ch));
+
+    // ...process $output now
+} catch(Exception $e) {
+
+    trigger_error(sprintf(
+        'Curl failed with error #%d: %s',
+        $e->getCode(), $e->getMessage()),
+        E_USER_ERROR);
+}
+?>
